abb4eeffadccc46d3d44cea902c4d75420667a38
jensnti <jens.andreasson@ga.ntig.se>
2020-02-06 15:06:11 +0100

    fixes

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,14 @@
+const kolgra = "rgba(34,34,34,0.6)";
+const magenta = "rgba(75,0,130,0.45)";
+const ultraviolett = "rgba(221,8,144,0.2)";
+const font = "12px Open Sans";
+
 const Background = function(groundPoint) {
     const background = {};
     background.ground = groundPoint + 15;
-    background.skyColor = "rgba(221,8,144,0.25)";
-    background.groundStroke = "rgba(34,34,34,0.55)";
-    background.groundColor = "rgba(75,0,130,0.45)";
+    background.skyColor = ultraviolett;
+    background.groundStroke = kolgra;
+    background.groundColor = magenta;
 
     background.draw = function(ctx, cWidth, cHeight) {
         ctx.fillStyle = this.skyColor;
@@ -24,7 +29,7 @@ const Circle = function(x, y, r) {
     circle.x = x;
     circle. y = y;
     circle.r = r;
-    circle.strokeColor = "rgba(34,34,34,0.25)";
+    circle.strokeColor = kolgra;
 
     circle.update = function(x, y) {
         this.x = x;
@@ -45,8 +50,8 @@ const Arrow = function() {
     arrow.drawn = false;
     arrow.fired = false;
     arrow.aim = {x: 0, y: 0};
-    arrow.strokeColor = "rgba(34,34,34,0.35)";
-    arrow.fillColor = "rgba(34,34,34,0.55)";
+    arrow.strokeColor = kolgra;
+    arrow.fillColor = kolgra;
     arrow.headLength = 8;
     arrow.angle = 0;
 
@@ -104,8 +109,8 @@ const Arrow = function() {
             ctx.fillStyle = this.fillColor;
             ctx.fill();
 
-            ctx.font = "10px Arial";
-            ctx.fillStyle = "rgba(34,34,34,0.85)";
+            ctx.font = font;
+            ctx.fillStyle = kolgra;
             ctx.fillText(
                 Math.round(this.angle * (180 / Math.PI))
                 + "° Velocity: " + Math.round(distanceBetween(circle, mouse) / 6)
@@ -127,8 +132,8 @@ const Projectile = function(x, y, angle, velocity) {
     projectile.velX = velocity * Math.cos(angle);
     projectile.velY = velocity * Math.sin(angle);
     projectile.increment = 0.2;
-    projectile.strokeColor = "rgba(34,34,34,0.65)";
-    projectile.fillColor = "rgba(34,34,34,0.65)";
+    projectile.strokeColor = kolgra;
+    projectile.fillColor = kolgra;
     projectile.r = 4;
     projectile.lastX = 0;
     projectile.lastY = 0;
@@ -136,42 +141,19 @@ const Projectile = function(x, y, angle, velocity) {
     projectile.length = 28;
 
     projectile.update = function(gravity, groundPoint) {
-
-//        this.angle = this.angle * this.increment;
-
-        if (this.y <= groundPoint) {
+        if (this.y < groundPoint - 6) {
             this.lastX = this.x;
             this.lastY = this.y;
-            // // this.angle = this.angle + this.increment;
-
-            // let cos = Math.cos(this.angle);
-            // let sin = Math.sin(this.angle);
     
             this.x = this.x + this.velX * this.increment;
             this.y = this.y + this.velY * this.increment;
             this.velY = this.velY + gravity * this.increment * 0.1;
 
             this.angle = angleBetween({x: this.lastX, y: this.lastY}, {x: this.x, y: this.y});
-
-            console.log("a: " + this.angle * 180 / Math.PI)
-
         }
-
-        // var cos = Math.cos(angle),
-        // sin = Math.sin(angle);
-
-        // return {
-        //     x: cos -sin,
-        //     y: sin + cos
-        // }
     }
 
     projectile.draw = function(ctx) {
-        // ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        // ctx.fillStyle = this.fillColor;
-        // ctx.fill();
-
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.x + this.length * Math.cos(this.angle), this.y + this.length * Math.sin(this.angle));
@@ -202,9 +184,8 @@ const Projectile = function(x, y, angle, velocity) {
         ctx.fillStyle = this.fillColor;
         ctx.fill();
 
-
-        ctx.font = "10px Arial";
-        ctx.fillStyle = "rgba(34,34,34,0.85)";
+        ctx.font = font;
+        ctx.fillStyle = kolgra;
         ctx.fillText("X: " + Math.round(this.x) + " Y: " + Math.round(this.y), this.x + 8, this.y - 4);
     }
     return projectile;
@@ -213,6 +194,7 @@ const Projectile = function(x, y, angle, velocity) {
 window.addEventListener('load', (e) => {
     console.log('page is fully loaded');
 
+    // Skapa 2 canvas, fg och bg och lägg till dem i #stage
     const bgCanvas = document.createElement('canvas');
     bgCanvas.width = window.innerWidth;
     bgCanvas.height = window.innerHeight;
@@ -230,20 +212,25 @@ window.addEventListener('load', (e) => {
     stage.appendChild(bgCanvas);
     stage.appendChild(fgCanvas);
 
+    // sätt gravitationen samt markens punkt
     let gravity = 15;
     let groundPoint = cHeight - (cHeight / 4);
 
+    // ny bakgrund
     let bg = Background(groundPoint);
 
+    // skapa cirklarna för musens sikte
     let shootingCircle = Circle(200, groundPoint - 200, 75);
     let aimCircle = Circle(shootingCircle.x, shootingCircle.y, 10);
+    
+    // pilen, siktet samt avfyrade projektiler
+    let projectiles = [];
+    let arrow;
 
     // Mouse events
     let mousePos = {x: 0, y: 0};
     let savedMousePos;
     let mouseDown = false;
-    let projectiles = [];
-    let arrow;
 
     window.addEventListener('mousemove', (e) => {
         mousePos = getMousePos(e);
@@ -278,8 +265,6 @@ window.addEventListener('load', (e) => {
 
     function main() {
         fgCtx.clearRect(0, 0, cWidth, cHeight);
-        // fgCtx.fillStyle = "rgba(255,255,255,0.1)";
-        // fgCtx.fillRect(0, 0, cWidth, cHeight);
         bgCtx.clearRect(0, 0, cWidth, cHeight);
         bg.draw(bgCtx, cWidth, cHeight);
         shootingCircle.draw(fgCtx);
@@ -309,8 +294,6 @@ window.addEventListener('load', (e) => {
             shootingCircle.update(mousePos.x, mousePos.y);
             aimCircle.update(mousePos.x, mousePos.y);
         }
-
-
 
         for (let projectile of projectiles) {
             projectile.update(gravity, groundPoint);
